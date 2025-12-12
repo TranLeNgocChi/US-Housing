ae124ab47c74dd7c809bc065b16aed59ab8ea024
TranLeNgocChi <ngochi090304@gmail.com>
2025-12-12 17:39:33 +0700

    Add files via upload

---
+++ US house processing.sql
@@ -0,0 +1,182 @@
+﻿ALTER TABLE Thao_2_Zillow_table_backup
+ADD House_ID NVARCHAR(20);
+
+CREATE PROCEDURE UpHID
+AS
+BEGIN
+    DECLARE @base_url VARCHAR(100) = 'https://www.zillow.com/homedetails/';
+    
+    -- House ID
+    UPDATE Thao_2_Zillow_table_backup
+    SET House_ID = SUBSTRING(Links, 
+                              CHARINDEX('/', Links, LEN(@base_url) + 1) + 1,
+                              CHARINDEX('_zpid', Links) - CHARINDEX('/', Links, LEN(@base_url) + 1) - 1)
+    WHERE CHARINDEX('_zpid', Links) > 0;
+END;
+
+EXEC UpHID;
+
+--delete duplicate
+CREATE PROCEDURE RemoveDupHID1
+AS
+BEGIN
+    SELECT House_ID, COUNT(*) as count
+    FROM Thao_2_Zillow_table_backup
+    GROUP BY House_ID
+    HAVING COUNT(*) > 1;
+
+    SELECT 
+        COUNT(*) as total_rows,
+        COUNT(DISTINCT House_ID) as house_ids,
+        (SELECT SUM(duplicate_count - 1)
+         FROM (
+             SELECT COUNT(*) as duplicate_count
+             FROM Thao_2_Zillow_table_backup
+             GROUP BY House_ID
+             HAVING COUNT(*) > 1
+         ) t) as extra_duplicates
+    FROM Thao_2_Zillow_table_backup;
+
+    WITH DuplicateCTE AS (
+        SELECT *,
+            ROW_NUMBER() OVER (PARTITION BY House_ID ORDER BY House_ID) as row_num
+        FROM Thao_2_Zillow_table_backup
+    )
+    DELETE FROM DuplicateCTE 
+    WHERE row_num > 1;
+
+----Retype
+    ALTER TABLE Thao_2_Zillow_table_backup
+    ALTER COLUMN House_ID varchar(20) NOT NULL;
+END;
+EXEC RemoveDupHID1;
+
+
+--split
+ALTER TABLE Thao_2_Zillow_table_backup
+    ADD House_address VARCHAR(255), 
+        City VARCHAR(100), 
+        House_state CHAR(2), 
+        Zipcode CHAR(5),
+        Bedrooms NVARCHAR(50),
+        Bathrooms NVARCHAR(50),
+        Area NVARCHAR(50);
+
+CREATE PROCEDURE SplitAandR
+AS
+BEGIN
+    
+---address
+    UPDATE Thao_2_Zillow_table_backup
+    SET 
+        House_address = CASE 
+            WHEN CHARINDEX(',', [Address]) > 0 
+            THEN SUBSTRING([Address], 1, CHARINDEX(',', [Address]) - 1) 
+            ELSE NULL 
+        END,
+        
+        City = CASE 
+            WHEN LEN([Address]) - LEN(REPLACE([Address], ',', '')) >= 2 
+            THEN SUBSTRING([Address], CHARINDEX(',', [Address]) + 2, 
+                           CHARINDEX(',', [Address], CHARINDEX(',', [Address]) + 1) - CHARINDEX(',', [Address]) - 2) 
+            ELSE NULL 
+        END,
+        
+        House_state = CASE 
+            WHEN LEN([Address]) - LEN(REPLACE([Address], ',', '')) >= 2 
+            THEN SUBSTRING([Address], CHARINDEX(',', [Address], CHARINDEX(',', [Address]) + 1) + 2, 2) 
+            ELSE NULL 
+        END,
+        
+        Zipcode = CASE 
+            WHEN LEN([Address]) >= 5 
+            THEN RIGHT([Address], 5) 
+            ELSE NULL 
+        END;
+
+---room
+    UPDATE Thao_2_Zillow_table_backup
+    SET 
+        Bedrooms = CASE 
+            WHEN CHARINDEX('bds', [Details]) > 0 
+            THEN LEFT([Details], CHARINDEX('bds', [Details]) - 1) 
+            ELSE NULL 
+        END,
+        
+        Bathrooms = CASE 
+            WHEN CHARINDEX('ba', [Details]) > 0 AND CHARINDEX('bds', [Details]) > 0 
+            THEN SUBSTRING([Details], CHARINDEX('bds', [Details]) + 3, 
+                           CHARINDEX('ba', [Details]) - CHARINDEX('bds', [Details]) - 3) 
+            ELSE NULL 
+        END,
+        
+        Area = CASE 
+            WHEN CHARINDEX('sqft', [Details]) > 0 AND CHARINDEX('ba', [Details]) > 0 
+            THEN REPLACE(SUBSTRING([Details], 
+                     CHARINDEX('ba', [Details]) + 2, 
+                     CHARINDEX('sqft', [Details]) - CHARINDEX('ba', [Details]) - 2), ',', ' ')
+            ELSE NULL 
+        END;
+END;
+
+EXEC SplitAandR;
+
+--delete $
+CREATE PROCEDURE Remove$
+AS
+BEGIN
+    -- Cập nhật cột Price để xóa ký tự $ và dấu phẩy ,
+    UPDATE Thao_2_Zillow_table_backup
+    SET Price = REPLACE(REPLACE(Price, '$', ''), ',', '');
+END;
+
+EXEC Remove$;
+
+CREATE PROCEDURE Clean
+AS
+BEGIN
+--- delete null
+    DECLARE @nnull NVARCHAR(MAX);
+    SELECT @nnull = ISNULL(@nnull + ' OR ', '') + '[' + COLUMN_NAME + '] IS NULL'
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'Thao_2_Zillow_table_backup'; 
+    
+    SET @nnull = 'DELETE FROM [dbo].[Thao_2_Zillow_table_backup] WHERE ' + @nnull;
+    EXEC sp_executesql @nnull;
+
+ --- delete '--'
+    DECLARE @nnull2 NVARCHAR(MAX);
+    SELECT @nnull2 = ISNULL(@nnull2 + ' OR ', '') + '[' + COLUMN_NAME + '] LIKE ''%--%'''
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'Thao_2_Zillow_table_backup'; 
+    
+    SET @nnull2 = 'DELETE FROM [dbo].[Thao_2_Zillow_table_backup] WHERE ' + @nnull2;
+    EXEC sp_executesql @nnull2;
+
+ --- Not int
+    DELETE FROM [dbo].[Thao_2_Zillow_table_backup]  
+    WHERE 
+        TRY_CAST(Bedrooms AS INT) IS NULL OR 
+        TRY_CAST(Bathrooms AS INT) IS NULL OR 
+        TRY_CAST(Price AS INT) IS NULL;  
+END;
+EXEC Clean;
+--
+CREATE PROCEDURE DataColumn1
+AS
+BEGIN
+    ALTER TABLE [dbo].[Thao_2_Zillow_table_backup]
+    DROP COLUMN [Address], [Details], [Links];
+
+    -- data type
+    ALTER TABLE [dbo].[Thao_2_Zillow_table_backup]
+    ALTER COLUMN [Bedrooms] INT;
+
+    ALTER TABLE [dbo].[Thao_2_Zillow_table_backup]
+    ALTER COLUMN [Bathrooms] INT;
+
+    ALTER TABLE [dbo].[Thao_2_Zillow_table_backup]
+    ALTER COLUMN [Price] INT;
+END;
+
+EXEC DataColumn1;
---
+++ US_house_processing.sql
@@ -0,0 +1,243 @@
+﻿--House_ID
+CREATE PROCEDURE Add_House_ID
+AS
+BEGIN
+    ALTER TABLE [dbo].[Zillow_table]
+    ADD House_ID VARCHAR(50);
+END
+
+EXEC Add_House_ID
+
+CREATE PROCEDURE Update_House_ID
+AS
+BEGIN
+    UPDATE [dbo].[Zillow_table]
+    SET House_ID =	RIGHT(
+						LEFT(Links, CHARINDEX('_zpid', Links) - 1), 
+						CHARINDEX('/', 
+							REVERSE(
+								LEFT(Links, CHARINDEX('_zpid', Links) - 1)
+							)
+						) - 1
+					);
+END
+
+EXEC Update_House_ID
+
+--duplicate
+CREATE PROCEDURE Delete_Duplicates
+AS
+BEGIN
+    WITH CTE_Duplicates AS (
+        SELECT *, 
+			   ROW_NUMBER() OVER (
+					PARTITION BY House_ID
+					ORDER BY (SELECT NULL)
+				) AS rn
+        FROM [dbo].[Zillow_table]
+    )
+    DELETE FROM CTE_Duplicates 
+    WHERE rn > 1;
+END
+
+EXEC Delete_Duplicates
+
+
+--Address
+CREATE PROCEDURE Create_Adr
+AS
+BEGIN
+	ALTER TABLE [dbo].[Zillow_table]
+	ADD Street_adr	NVARCHAR(255),
+		City_adr	NVARCHAR(255),
+		State_adr	NVARCHAR(50),
+		Zip_Code	NVARCHAR(10);
+END
+
+EXEC Create_Adr
+
+CREATE PROCEDURE Update_adr
+AS
+BEGIN
+    UPDATE [dbo].[Zillow_table]
+    SET 
+        -- Street
+        Street_adr = CASE 
+						WHEN CHARINDEX(',', Address) > 0 
+						THEN LEFT(Address, CHARINDEX(',', Address) - 1) 
+						ELSE Address 
+					 END,
+
+        -- City
+        City_adr =	CASE 
+						WHEN CHARINDEX(',', Address) > 0 
+							 AND CHARINDEX(',', Address, CHARINDEX(',', Address) + 1) > 0 
+						THEN LTRIM(
+								SUBSTRING(
+									Address, 
+									CHARINDEX(',', Address) + 1, 
+									CHARINDEX(',', Address, CHARINDEX(',', Address) + 1) - CHARINDEX(',', Address) - 1
+								)
+							 ) 
+						ELSE NULL 
+					END,
+
+        -- State
+        State_adr = CASE 
+						WHEN CHARINDEX(',', Address, CHARINDEX(',', Address) + 1) > 0 
+						THEN LTRIM(
+								SUBSTRING(
+									Address, 
+									CHARINDEX(',', Address, CHARINDEX(',', Address) + 1) + 1,
+									3
+								)
+							 ) 
+						ELSE NULL 
+					END,
+
+        --  Zip Code
+        Zip_Code = CASE 
+                      WHEN LEN(Address) >= 5 
+                      THEN RIGHT(Address, 5) 
+                      ELSE NULL 
+                   END;
+END;
+
+EXEC Update_adr
+
+--Detail
+--
+CREATE PROCEDURE Create_details
+AS
+BEGIN
+    ALTER TABLE [dbo].[Zillow_table]
+    ADD Bedrooms		INT,
+        Bathrooms		INT,
+        Square_Footage	INT;
+END
+
+EXEC Create_details
+
+--
+CREATE PROCEDURE Update_Details
+AS
+BEGIN
+    UPDATE [dbo].[Zillow_table]
+    SET 
+        --Bedrooms
+        Bedrooms =	CASE 
+						WHEN (CHARINDEX(' bds', Details) > 0 OR CHARINDEX(' bd', Details) > 0) 
+							 AND ISNUMERIC(LEFT(Details, CHARINDEX(' ', Details) - 1)) = 1
+						THEN TRY_CAST(
+								LTRIM(RTRIM(
+										LEFT(Details, CHARINDEX(' ', Details) - 1)
+									  )
+								) AS INT
+							 )
+						ELSE 
+							CASE 
+								WHEN CHARINDEX(' ba', Details) > 0 
+										AND LEFT(Details, CHARINDEX(' ba', Details) - 1) LIKE '%Studio%' 
+								THEN 0
+								ELSE NULL
+							END
+						END,
+
+        -- Bathrooms
+        Bathrooms = CASE 
+						WHEN CHARINDEX(' ba', Details) > 0 
+						THEN TRY_CAST(
+								SUBSTRING(
+									Details, 
+									CHARINDEX(' ba', Details) - 1, 
+									1
+								) AS INT
+							 )
+						ELSE NULL 
+					END,
+
+        -- Square Footage
+        Square_Footage = CASE 
+							WHEN CHARINDEX('sqft', Details) > 0 
+								 AND CHARINDEX(' ba', Details) > 0 
+							THEN 
+								CASE 
+									WHEN SUBSTRING(
+											Details, 
+											CHARINDEX(' ba', Details) + 3, 
+											LEN(Details)
+										 ) LIKE '%--%' 
+									THEN NULL
+									ELSE TRY_CAST(
+											REPLACE(	
+												LTRIM(RTRIM(
+														SUBSTRING(
+															Details, 
+															CHARINDEX(' ba', Details) + 3, 
+															CHARINDEX('sqft', Details) - CHARINDEX(' ba', Details) - 3
+														)
+												)),
+												',', 
+												''
+											) AS INT
+										 )
+								END
+							ELSE NULL 
+						 END
+    WHERE 
+        Details IS NOT NULL;
+END;
+
+EXEC Update_Details
+
+
+--Price
+CREATE PROCEDURE Update_Price
+AS
+BEGIN
+    UPDATE [dbo].[Zillow_table]
+    SET Price = CASE
+					WHEN CHARINDEX('.', Price) > 0 
+						 OR CHARINDEX('--', Price) > 0 
+					THEN NULL                            
+					ELSE REPLACE(
+							REPLACE(
+								REPLACE(
+									REPLACE(
+										REPLACE(
+											Price, 
+											'$', ''
+										),
+										',', ''
+									),
+									'K', '000'
+								),
+								'+', ''
+							),
+							'--', ''
+						)								
+				END;
+
+    ALTER TABLE [dbo].[Zillow_table]
+    ALTER COLUMN Price INT;
+END;
+
+EXEC Update_Price
+
+--Null
+CREATE PROCEDURE Delete_Null
+AS
+BEGIN
+    DELETE FROM [dbo].[Zillow_table]
+    WHERE Price			  IS NULL
+	   OR House_ID		 IS NULL
+       OR Street_adr	 IS NULL
+       OR City_adr		 IS NULL
+       OR State_adr		 IS NULL
+       OR Zip_Code		 IS NULL
+       OR Bedrooms		 IS NULL
+       OR Bathrooms		 IS NULL
+       OR Square_Footage IS NULL; 
+END;
+
+EXEC Delete_Null
